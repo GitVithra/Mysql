f9b9605394e22a573f043a9d1b0989e2afb131cf
Pavithra Neerudi <99332810+GitVithra@users.noreply.github.com>
2022-02-22 17:36:19 +0530

    Add files via upload

---
+++ test.sql
@@ -0,0 +1,17 @@
+create database demo;
+
+
+create table test (
+Name varchar(20), 
+Age int, 
+Gender varchar (20));
+
+insert  into test values("Pavithra", 21, "Female");
+insert into test values("Srikanth",24,"Male");
+insert into test values("Yevan",19, "Male");
+insert into test values("Akshaya", 15, "Famale");
+insert into test  values("Harika",18,"Female");
+
+select * from test;
+
+ 
